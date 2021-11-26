e165a9fe54cfb0aedd47bb2ec586501a28c6753e
SephorasLand <rperedah@gmail.com>
2021-11-26 18:51:34 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,35 @@
+function showMore1(){
+    document.getElementById("entrada1").style.display = "block";
+    document.getElementById("enlaceEntrada1mas").style.display = "none";
+    document.getElementById("enlaceEntrada1menos").style.display = "block";
+}
+
+function showLess1(){
+    document.getElementById("entrada1").style.display = "none";
+    document.getElementById("enlaceEntrada1mas").style.display = "block";
+    document.getElementById("enlaceEntrada1menos").style.display = "none";
+}
+
+function showMore2(){
+    document.getElementById("entrada2").style.display = "block";
+    document.getElementById("enlaceEntrada2mas").style.display = "none";
+    document.getElementById("enlaceEntrada2menos").style.display = "block";
+}
+
+function showLess2(){
+    document.getElementById("entrada2").style.display = "none";
+    document.getElementById("enlaceEntrada2mas").style.display = "block";
+    document.getElementById("enlaceEntrada2menos").style.display = "none";
+}
+
+function showMore3(){
+    document.getElementById("entrada3").style.display = "block";
+    document.getElementById("enlaceEntrada3mas").style.display = "none";
+    document.getElementById("enlaceEntrada3menos").style.display = "block";
+}
+
+function showLess3(){
+    document.getElementById("entrada3").style.display = "none";
+    document.getElementById("enlaceEntrada3mas").style.display = "block";
+    document.getElementById("enlaceEntrada3menos").style.display = "none";
+}
